49cd1661ded1f28046704390a520a9d01a6a43f6
ByMikiii <75531503+ByMikiii@users.noreply.github.com>
2022-06-07 22:46:48 +0200

    Added DB 👝

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 import fetch from 'node-fetch';
 import queryString from 'query-string';
+import mysql from 'mysql';
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "TwitchLoyalty"
+});
 
 import { clientId, secret, accessToken } from './settings.js';
 
 var isLive;
-
+var listOfUsers;
 
 function appRunning() {
-  var streamerName = 'donatolive';
+  var streamerName = 'resttpowered';
 
   //CHECKS IF STREAMER IS STREAMING
   fetch('https://api.twitch.tv/helix/search/channels?query=' + streamerName, {
@@ -40,11 +47,10 @@ function appRunning() {
                 var json = data;
                 var obj = JSON.parse(json);
 
-                for (var i = 0; i < obj.chatters['viewers'].length; i++) {
-                  console.log(obj.chatters['viewers'][i]);
-                }
-
+                listOfUsers = obj.chatters['viewers'];
                 console.log('Number of chatters: ' + obj.chatter_count);
+
+                insertUser(0);
 
               })
 
@@ -55,3 +61,38 @@ function appRunning() {
 
 
 appRunning();
+
+
+
+function insertUser(value) {
+  var recentUsername = listOfUsers[value];
+
+  //CREATES USER IN DATABASE
+  con.connect(function (err) {
+    var sql = "INSERT INTO users (username, points) VALUES ('" + recentUsername + "', 1)";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log(recentUsername + ' added.');
+    }
+    )
+  }
+  )
+
+  if (value + 1 < listOfUsers.length) {
+    setTimeout(() => {
+      value++;
+      insertUser(value);
+    }, 10)
+  }
+}
+
+
+// create database TwitchLoyalty;
+// use TwitchLoyalty;
+// create table users
+// (
+//     id       int auto_increment
+//         primary key,
+//     username varchar(64) null,
+//     points   int         null
+// );
