8188c2091caee7cb323534f1339bc20f8764a6b7
ByMikiii <75531503+ByMikiii@users.noreply.github.com>
2022-07-02 19:42:09 +0200

    Added tailwindconfig file 🤟

---
+++ Website/loyalty/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.php",
+    "./parts/*.php",
+    "./leaderboard/index.php",
+    "./manager/index.php",
+    "./profile/index.php",
+    "./manager/create/index.php",
+    "./manager/redeems/index.php"],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: {
+          '100': '#130a3e'
+        },
+        redpink: {
+          '100': 'e33146'
+        }
+
+      }
+    },
+  },
+  plugins: [],
+}
